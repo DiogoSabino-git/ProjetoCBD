142037b4fa292d5be5800ee9b5e5b369d1f8f008
DiogoSabino-git <202300149@estudantes.ips.pt>
2025-10-28 19:06:58 +0000

    Regras de Negócio / MER (Ficheiro Word)

---
+++ Queries.sql
@@ -0,0 +1,28 @@
+select * from SalesTerritory;
+
+select * from Sales;
+
+select * from Currency;
+
+select * from Products;
+
+select * from ProductSubCategory;
+
+select * from SalesTerritory;
+
+select * from Customer;
+
+select COUNT(*) from Sales;
+
+select COUNT(SalesTerritoryKey) from SalesTerritory;
+
+select COUNT(Title) from Customer;
+
+select * from Customer
+where Title is not null;
+
+select * from Sales
+where UnitPrice != TotalSalesAmount;
+
+select * from Sales
+where SalesOrderLineNumber != 1;
