4d4e0b2cbb86376f6f944863f29757910c1f26e2
DiogoSabino-git <202300149@estudantes.ips.pt>
2025-12-18 16:12:36 +0000

    Tabela UserManagement.AccessLogs

---
+++ conflict.sql
@@ -0,0 +1,16 @@
+-- Diogo Sabino nº202300149
+-- Rodrigo Antunes nº2024151048
+--
+-- Script Conflict
+
+USE AdventureWorks;
+GO
+
+-- Garantir que o produto 1 tem o preço base de 100.00
+UPDATE Production.Products
+SET listPrice = 100.00
+WHERE productID = 1;
+
+SELECT productID, productName, listPrice 
+FROM Production.Products 
+WHERE productID = 1;
